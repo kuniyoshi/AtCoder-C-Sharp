031263e850da2ae940675b7795278814ba7751f9
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2021-11-16 22:20:07 +0900

    227.C: AC

--- c227/C.cs
+++ c227/C.cs
@@ -1,6 +1,5 @@
 ﻿#nullable enable
 using System;
-using System.Collections.Generic;
 
 namespace AtCoder.c227
 {
@@ -9,70 +8,20 @@ namespace AtCoder.c227
         internal static void Run()
         {
             var n = long.Parse(Console.ReadLine()!);
-            var primes = new List<long>();
-            var furui = new bool[((long)Math.Sqrt(n)) + 2];
 
-            if (furui.Length < 2)
+            var count = 0L;
+
+            for (var a = 1L; a * a * a <= n; ++a)
             {
-                Console.WriteLine(1);
-                return;
-            }
-
-            for (var i = 0; i < furui.Length; ++i)
-            {
-                furui[i] = true;
-            }
-
-            furui[1] = false;
-
-            for (var i = 2; i < furui.Length; ++i)
-            {
-                if (furui[i])
+                for (var b = a; a * b * b <= n; ++b)
                 {
-                    if ((n % i) == 0)
-                    {
-                        primes.Add(i);
-                    }
-
-                    for (var j = 2 * i; j < furui.Length; j += i)
-                    {
-                        furui[j] = false;
-                    }
+                    var maxC = n / (a * b);
+                    var patterns = maxC - b + 1;
+                    count = count + patterns;
                 }
             }
 
-            Console.WriteLine(string.Join(", ", primes));
-            Console.WriteLine(primes.Count);
-
-            // var count = new long[primes.Count];
-            var items = 0L;
-
-            for (var i = 0; i < primes.Count; ++i)
-            {
-                var x = n;
-
-                while ( x % primes[i] ==0)
-                {
-                    x /= primes[i];
-                    items++;
-                }
-                // count[i] = n / primes[i];
-                // items += n / primes[i];
-            }
-
-            var a = items;
-            var b = items - 1;
-
-            if (a % 2 == 0)
-            {
-                a /= 2;
-            }
-            else
-            {
-                b /= 2;
-            }
-
-            Console.WriteLine(a * b);
+            Console.WriteLine(count);
         }
     }
 }
