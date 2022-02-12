850d036f2462d6cdb4b345a7f2311d6ae542509f
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-02-12 20:58:03 +0900

    238.d: wip

--- c238/D.cs
+++ c238/D.cs
@@ -6,8 +6,6 @@ namespace AtCoder.c238
 {
     internal static class D
     {
-        static bool[,,,,] Can { get; }
-
         internal static void Run()
         {
             var t = int.Parse(Console.ReadLine()!);
@@ -16,52 +14,47 @@ namespace AtCoder.c238
 
             foreach (var query in q)
             {
-                var has = Test(query[0], query[1], increment: 0);
+                var has = Test(query[0], query[1]);
                 Console.WriteLine(has ? "Yes" : "No");
             }
         }
 
-        static bool Test(long and, long sum, int increment)
+        static bool Test(long and, long sum)
         {
-            if (and == 0 && sum == 0)
+            if (sum == 0)
             {
-                return Can[and, sum, increment, 0, 0];
+                return and == 0;
             }
 
             var a = and & 1;
-            var s = sum & 1;
-            var can = false;
 
             for (var x = 0; x < 2; ++x)
             {
                 for (var y = 0; y < 2; ++y)
                 {
-                    if (!Can[a, s, increment, x, y])
+                    if ((x & y) != a)
                     {
                         continue;
                     }
 
-                    if (Test(and >> 1, sum >> 1, (increment + x + y) >> 1))
+                    if ((sum - x - y) < 0)
                     {
-                        can = true;
+                        continue;
+                    }
+
+                    if ((sum - x - y) % 2 != 0)
+                    {
+                        continue;
+                    }
+
+                    if (Test(and >> 1, (sum - x - y) >> 1))
+                    {
+                        return true;
                     }
                 }
             }
 
-            return can;
-        }
-
-        static D()
-        {
-            Can = new bool[2, 2, 2, 2, 2];
-            Can[0, 0, 0, 0, 0] = true;
-            Can[0, 0, 1, 0, 1] = true;
-            Can[0, 0, 1, 1, 0] = true;
-            Can[0, 1, 0, 0, 1] = true;
-            Can[0, 1, 0, 1, 0] = true;
-            Can[0, 1, 1, 0, 0] = true;
-            Can[1, 0, 0, 1, 1] = true;
-            Can[1, 1, 1, 1, 1] = true;
+            return false;
         }
     }
 }
