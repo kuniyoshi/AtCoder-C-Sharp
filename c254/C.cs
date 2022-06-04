e87c5bd2d0ea6b82e02d865892519f6a488a9b7d
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-06-04 22:52:46 +0900

    254: while contest

---
+++ c254/C.cs
@@ -0,0 +1,193 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AtCoder.c254
+{
+    internal static class C
+    {
+        internal static void Run()
+        {
+            var nk = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+            var (n, k) = (nk[0], nk[1]);
+            var a = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+            var queues = MakeQueues(k, a);
+
+            while (queues[0].Any())
+            {
+                for (var i = 0; i < queues.Length - 1; ++i)
+                {
+                    if (!queues[i + 1].Any())
+                    {
+                        continue;
+                    }
+
+                    if (queues[i].Peek() > queues[i + 1].Peek())
+                    {
+                        Console.WriteLine("No");
+                        return;
+                    }
+                }
+
+                for (var i = 0; i < queues.Length; ++i)
+                {
+                    if (!queues[i].Any())
+                    {
+                        continue;
+                    }
+
+                    queues[i].Dequeue();
+                }
+            }
+            
+            Console.WriteLine("Yes");
+        }
+
+        static Queue<int>[] MakeQueues(int k, int[] a)
+        {
+            var intermediate = Enumerable.Range(0, k)
+                .Select(_ => new List<int>())
+                .ToArray();
+
+            for (var i = 0; i < a.Length; ++i)
+            {
+                intermediate[i % k].Add(a[i]);
+            }
+
+            var queues = new Queue<int>[k];
+
+            for (var i = 0; i < intermediate.Length; ++i)
+            {
+                queues[i] = new Queue<int>(intermediate[i].OrderBy(x => x));
+            }
+
+            return queues;
+        }
+
+        internal class LowerPriorQueue
+        {
+            List<int> Items { get; } = new List<int>();
+
+            internal bool Any()
+            {
+                return Items.Any();
+            }
+
+            internal int Peek()
+            {
+                return Items[index: 0];
+            }
+
+            internal int Pop()
+            {
+                Debug.Assert(Items.Any(), "Items.Any()");
+                return Heap.ReversePopFrom(Items);
+            }
+
+            internal void Push(int value)
+            {
+                Heap.ReversePushTo(Items, value);
+            }
+        }
+
+        internal static class Heap
+        {
+            internal static int PopFrom(List<int> buffer)
+            {
+                Debug.Assert(buffer.Any(), "buffer.Any()");
+                var lastRoot = buffer[index: 0];
+                buffer[index: 0] = buffer[buffer.Count - 1];
+                buffer.RemoveAt(buffer.Count - 1);
+
+                var cursor = 0;
+                int left;
+
+                while ((left = 2 * cursor + 1) < buffer.Count)
+                {
+                    var right = left + 1;
+
+                    var child = right < buffer.Count && buffer[left] <= buffer[right]
+                        ? right
+                        : left;
+
+                    if (buffer[cursor] < buffer[child])
+                    {
+                        (buffer[cursor], buffer[child]) = (buffer[child], buffer[cursor]);
+                    }
+
+                    cursor = child;
+                }
+
+                return lastRoot;
+            }
+
+            internal static void PushTo(List<int> buffer, int item)
+            {
+                buffer.Add(item);
+                var cursor = buffer.Count - 1;
+
+                while (cursor != 0)
+                {
+                    var parent = (cursor - 1) / 2;
+
+                    if (buffer[parent] < buffer[cursor])
+                    {
+                        (buffer[parent], buffer[cursor]) = (buffer[cursor], buffer[parent]);
+                    }
+
+                    cursor = parent;
+                }
+            }
+
+            internal static int ReversePopFrom(List<int> buffer)
+            {
+                Debug.Assert(buffer.Any(), "buffer.Any()");
+                var lastRoot = buffer[index: 0];
+                buffer[index: 0] = buffer[buffer.Count - 1];
+                buffer.RemoveAt(buffer.Count - 1);
+
+                var cursor = 0;
+                int left;
+
+                while ((left = 2 * cursor + 1) < buffer.Count)
+                {
+                    var right = left + 1;
+
+                    var child = right < buffer.Count && buffer[left] >= buffer[right]
+                        ? right
+                        : left;
+
+                    if (buffer[cursor] > buffer[child])
+                    {
+                        (buffer[cursor], buffer[child]) = (buffer[child], buffer[cursor]);
+                    }
+
+                    cursor = child;
+                }
+
+                return lastRoot;
+            }
+
+            internal static void ReversePushTo(List<int> buffer, int item)
+            {
+                buffer.Add(item);
+                var cursor = buffer.Count - 1;
+
+                while (cursor != 0)
+                {
+                    var parent = (cursor - 1) / 2;
+
+                    if (buffer[parent] > buffer[cursor])
+                    {
+                        (buffer[parent], buffer[cursor]) = (buffer[cursor], buffer[parent]);
+                    }
+
+                    cursor = parent;
+                }
+            }
+        }
+    }
+}
---
+++ c254/D.cs
@@ -0,0 +1,75 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+
+namespace AtCoder.c254
+{
+    internal static class D
+    {
+        internal static void Run()
+        {
+            var n = int.Parse(Console.ReadLine()!);
+            var partList = Enumerable.Range(0, n).Select(_ => new List<int>()).ToArray();
+
+            for (var i = 1; i <= n; ++i)
+            {
+                for (var j = i; j <= n; j += i)
+                {
+                    partList[i - 1].Add(j);
+                }
+            }
+
+            var isDouble = new HashSet<long>();
+
+            for (var i = 0; i < n; ++i)
+            {
+                isDouble.Add((long)(i + 1) * (i + 1));
+            }
+
+            var wholeFactors = new List<int>[n];
+            wholeFactors[0] = new List<int>{1};
+            wholeFactors[1] = new List<int>{2};
+            wholeFactors[2] = new List<int>{3};
+            wholeFactors[3] = new List<int>{2, 2};
+
+            var count = 0L;
+
+            for (var i = 0; i < partList.Length; ++i)
+            {
+                var factors = GetFactors(i, wholeFactors, partList[i]);
+                var values = factors.Aggregate(1L, (x, y) => x * y);
+                if (isDouble.Contains(values))
+                    count += (factors.Count + 1) * (factors.Count+1);
+            }
+
+            Console.WriteLine(count);
+        }
+
+        static List<int> GetFactors(int x, List<int>[] wholeFactors, List<int> pairs)
+        {
+            if (wholeFactors[x] != null)
+            {
+                return wholeFactors[x];
+            }
+
+            var factors = new List<int>();
+
+            for (var i = pairs.Count - 1; i >= 0; --i)
+            {
+                if (wholeFactors[i] != null)
+                {
+                    factors.AddRange(wholeFactors[i]);
+                    break;
+                }
+                
+                factors.Add(pairs[i]);
+            }
+
+            wholeFactors[x] = factors;
+
+            return factors;
+        }
+    }
+}
