d9bfc6a5f6b695fc360961dbbd37b4bb678dc75c
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-06-04 22:55:02 +0900

    add yes no lib

---
+++ Lib/YesNo.cs
@@ -0,0 +1,13 @@
+#nullable enable
+using System;
+
+namespace AtCoder.Lib
+{
+    internal static class YesNo
+    {
+        internal static void Write(bool isYes)
+        {
+            Console.WriteLine(isYes ? "Yes" : "No");
+        }
+    }
+}
