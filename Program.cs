94cc169b184766fb2aacf150bc999c97c93a786a
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-05-21 16:49:52 +0900

    165.b: ac

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace AtCoder
     {
         static void Main()
         {
-            c302.A.Run();
+            c165.B.Run();
         }
     }
 }
---
+++ c165/B.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+using System;
+
+namespace AtCoder.c165
+{
+    internal static class B
+    {
+        internal static void Run()
+        {
+            var x = long.Parse(Console.ReadLine()!);
+            var count = 0;
+            var acc = 100L;
+
+            while (acc < x)
+            {
+                acc += acc / 100;
+                count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
